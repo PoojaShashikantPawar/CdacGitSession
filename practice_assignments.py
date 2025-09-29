dd1ba0fcb044ceffafe177ea0c5e226091d7149f
PoojaShashikantPawar <134519716+PoojaShashikantPawar@users.noreply.github.com>
2025-09-29 15:14:41 +0530

    Add files via upload

---
+++ practice_assignments.py
@@ -0,0 +1,197 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+import math
+r = input("enter the radius of the circle")
+enter the radius of the circle 1.1
+
+#area of triangle
+import math
+r = float(input("enter the value of r ="))
+enter the value of r =1.1
+pi=math.pi
+area = pi*r*r
+print(pi)
+3.141592653589793
+math.sqrt(36)
+6.0
+
+
+f = float(input("enter the temperature in fahrenheit = "))
+enter the temperature in fahrenheit = 75
+c =(f-32)*(5/9)
+print(c)
+23.88888888888888
+               
+
+
+a =int(input("Enter value of a ="))
+             
+Enter value of a =5
+b =int(input("Enter value of b ="))
+             
+Enter value of b =6
+c =int(input("Enter value of c ="))
+             
+Enter value of c =7
+s=(a+b+c)
+             
+s=(a+b+c)/2
+             
+exist()
+             
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    exist()
+NameError: name 'exist' is not defined. Did you mean: 'exit'?
+area =(math.squrt(s(s-a)*(s-b)*(s-c))
+       print(area)
+       
+SyntaxError: '(' was never closed
+area = math.squrt(s(s-a)*(s-b)*(s-c))
+       
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    area = math.squrt(s(s-a)*(s-b)*(s-c))
+AttributeError: module 'math' has no attribute 'squrt'. Did you mean: 'sqrt'?
+area = math.squrt(s*(s-a)*(s-b)*(s-c))
+       
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    area = math.squrt(s*(s-a)*(s-b)*(s-c))
+AttributeError: module 'math' has no attribute 'squrt'. Did you mean: 'sqrt'?
+area =math.sqrt(s*(s-a)*(s-b)*(s-c))
+       
+print(area)
+       
+14.696938456699069
+x ="""Twinkle, twincle, little star,
+              How I wonder what you are!
+                      Up above the world so high,
+                      Like a diamond in the sky,
+      Twinkle,twinkle,little star,
+             How I wonder what you are"""
+print(x)
+       
+SyntaxError: multiple statements found while compiling a single statement
+x ="""Twinkle, twincle, little star,
+              How I wonder what you are!
+                      Up above the world so high,
+                      Like a diamond in the sky,
+      Twinkle,twinkle,little star,
+             How I wonder what you are"""
+       
+print(x)
+       
+Twinkle, twincle, little star,
+              How I wonder what you are!
+                      Up above the world so high,
+                      Like a diamond in the sky,
+      Twinkle,twinkle,little star,
+             How I wonder what you are
+
+import cmath
+a =int(input("Enter value of a =")
+       a=int(input("Enter value of a ="))
+       
+SyntaxError: '(' was never closed
+a =int(input("enter value of a ="))
+       
+enter value of a =5
+b =int(input("enter value of b ="))
+       
+enter value of b =6
+c =int(input("enter value of c ="))
+       
+enter value of c =7
+d=b*b-4*a*c
+       
+x1(-b+(cmath.sqrt(d))/2*a)
+       
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    x1(-b+(cmath.sqrt(d))/2*a)
+NameError: name 'x1' is not defined. Did you mean: 'x'?
+x =(-b+(cmath.sqrt(d))/2*a)
+       
+x1 =(-b-(cmath.sqrt(d))/2*a)
+       
+print(d)
+       
+-104
+print(x)
+       
+(-6+25.495097567963924j)
+print(x1)
+       
+(-6-25.495097567963924j)
+a =54321
+       
+sum = 0
+       
+sum =a%10+sum
+       
+b =a/10
+       
+print(int(b))
+       
+5432
+sum =a%10+sum
+       
+print(sum)
+       
+2
+print(a)
+       
+54321
+b =a/10
+       
+print(int(b))
+       
+5432
+sum =b%10+sum
+       
+c =a/10
+       
+print(int(c))
+       
+5432
+c =b/10
+       
+print(int(c))
+       
+543
+sum =c%10+sum
+...        
+>>> d =c/10
+...        
+>>> print(d)
+...        
+54.321000000000005
+>>> print(int(d))
+...        
+54
+>>> sum =d%10+sum
+...        
+>>> e =d/10
+...        
+>>> print(int(e))
+...        
+5
+>>> x="100"
+...        
+>>> print(x)
+...        
+100
+>>> x=("abc,abc,abc,abc,abc")
+...        
+>>> print(x)
+...        
+abc,abc,abc,abc,abc
+>>> line of dashes = '-'*50
+...        
+SyntaxError: invalid syntax
+>>> dashes ='-'*50
+...        
+>>> print(dashes)
+...        
+--------------------------------------------------
